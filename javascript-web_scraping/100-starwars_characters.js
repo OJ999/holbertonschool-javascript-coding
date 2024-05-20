476d1815f043726302f6a9a6b967eb5b8f501e75
Omar J <omarj819@gmail.com>
2024-05-20 11:59:12 +0300

    A

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
-
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
